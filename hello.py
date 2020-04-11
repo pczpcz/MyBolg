294f511a5094d28617bcad5e364c001ff493a867
Zhang Chuanpan <1099311614@qq.com>
2020-04-11 11:36:26 +0800

    web form test

--- hello.py
+++ hello.py
@@ -5,25 +5,41 @@ from flask import make_response
 from flask import redirect, url_for, abort
 from flask import render_template
 from flask_wtf import FlaskForm
-from wtforms imports StringField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app_ctx = app.app_context()
 app_ctx.push()
+bootstrap = Bootstrap(app)
+app.config['SECRET_KEY']='jdlfd jdklf dlla ld2oq n'
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-	name = request.args.get("name")
-	user_agent = request.headers.get("User_Agent")
-
-	return "<h1>hello world!, my name is {} from {}</h1></b> responed by {}" \
-		.format(name, user_agent, current_app.name)
+	name = None
+	form = User()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ""
+		return redirect(url_for(".user", name=name))
+	return render_template("index.html", name=name, form=form)
 
 @app.route("/user/<name>")
 def user(name):
-	response = make_response("<h2>response from server</h2>")
-	#abort(404)
-	#return redirect(url_for(".index"))
-	usr_info = ["zhangsan", 20]
-	return render_template("index.html", name=name, usr_info=usr_info)
+	return "<h1>hello, {}! </h1>".format(name)
+
+class User(FlaskForm):
+	name = StringField('User: ', validators=[DataRequired()])
+	submit = SubmitField('Submit')
+
+
+
+
+
+
+
+
+
+
+	
